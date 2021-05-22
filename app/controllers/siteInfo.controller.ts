7180e135b2ce274bdb5a08951380201648ffcac1
HelipengTony <tony.hlp@hotmail.com>
2021-05-22 17:24:59 +0800

    feat: snapod site

--- app/controllers/siteInfo.controller.ts
+++ app/controllers/siteInfo.controller.ts
@@ -15,66 +15,112 @@ BigInt.prototype.toJSON = function () {
 @Service()
 export class SiteInfoController {
 	@Post("/siteInfo")
-	async fetchRSSData(@Body() body: { customUrl: string, podcastId: number, episodeId: number }) {
+	async fetchRSSData(@Body() body: { customURL: string, podcastId: number, episodeId: number }) {
 
-		if (!body.customUrl && !body.podcastId && !body.episodeId) {
+		if (!body.customURL && !body.podcastId && !body.episodeId) {
 			throw new BadRequestError("Invalid request");
 		}
 
 		let result = null;
-		if (body.customUrl) {
-			result = await prisma.podcastProfile.findFirst({
-				where: {
-					snapod_site_custom_url: body.customUrl
-				},
-				include: {
-					podcast: {
-						include: {
-							author: {
-								select: {
-									name: true,
-									email: true
-								}
-							},
-							episodes: {
-								include: {
-									profile: true
-								}
+		if (body.customURL) {
+			if (body.episodeId) {
+				result = await prisma.podcastProfile.findUnique({
+					where: {
+						snapod_site_custom_url: body.customURL
+					},
+					select: {
+						podcast: {
+							select: {
+								name: true,
+								cuid: true,
+								profile: {
+									select: {
+										cover_art_image_url: true
+									}
+								},
+								episodes: {
+									where: {
+										id: body.episodeId
+									},
+									include: {
+										profile: true
+									}
+								},
+								author: true
 							}
 						}
-					}
-				},
-			});
-		} else if (body.episodeId) {
-			result = await prisma.episode.findUnique({
-				where: {
-					id: body.episodeId,
-				},
-				include: {
-					profile: true,
-					podcast: true
-				},
-			});
-		} else {
-			result = await prisma.podcast.findUnique({
-				where: {
-					id: body.podcastId,
-				},
-				include: {
-					profile: true,
-					author: {
-						select: {
-							name: true,
-							email: true
+					},
+				});
+			} else {
+				result = await prisma.podcastProfile.findFirst({
+					where: {
+						snapod_site_custom_url: body.customURL
+					},
+					include: {
+						podcast: {
+							include: {
+								author: {
+									select: {
+										name: true,
+										email: true
+									}
+								},
+								episodes: {
+									include: {
+										profile: true
+									}
+								},
+								profile: {
+									select: {
+										cover_art_image_url: true
+									}
+								},
+							}
 						}
 					},
-					episodes: {
-						include: {
-							profile: true
+				});
+			}
+		} else {
+			if (body.episodeId) {
+				result = await prisma.episode.findUnique({
+					where: {
+						id: body.episodeId,
+					},
+					include: {
+						profile: true,
+						podcast: {
+							include: {
+								author: true,
+								profile: {
+									select: {
+										cover_art_image_url: true
+									}
+								},
+							}
 						}
-					}
-				},
-			});
+					},
+				});
+			} else {
+				result = await prisma.podcast.findUnique({
+					where: {
+						id: body.podcastId,
+					},
+					include: {
+						profile: true,
+						author: {
+							select: {
+								name: true,
+								email: true
+							}
+						},
+						episodes: {
+							include: {
+								profile: true
+							}
+						}
+					},
+				});
+			}
 		}
 
 		return JSON.stringify(result);
